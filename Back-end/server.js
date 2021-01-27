4ee4905a626c7a2a4ede634eae75db085e081842
35_Shaima Win_SEI-14 <Shaimacs@yahoo.com>
2021-01-27 17:06:55 +0300

    Update the server

--- Back-end/server.js
+++ Back-end/server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 // Require Route Files
 const jobs = require('./route/jobs');
+const users = require('./route/users');
 // const articlesRouter = require('./routes/articles');
 
 // Require DB Configuration File
@@ -20,26 +21,18 @@ mongoose.connection.once('open', () => {
 // Instantiate Express Application Object
 const app = express();
 
-
-app.get('/', (req, res) => {
-  console.log('get /');
-  res.json('result');
-});
-
-
-
 /*** Middleware ***/
 
-// Add `bodyParser` middleware which will parse JSON requests
+// Add bodyParser middleware which will parse JSON requests
 // into JS objects before they reach the route files.
 //
-// The method `.use` sets up middleware for the Express application
+// The method .use sets up middleware for the Express application
 app.use(express.json());
 
 const reactPort = 5000;
-// Set CORS headers on response from this API using the `cors` NPM package.
+// Set CORS headers on response from this API using the cors NPM package.
 app.use(
-  cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}` })
+  cors({ origin: process.env.CLIENT_ORIGIN || http://localhost:${reactPort} })
 );
 
 /*** Routes ***/
@@ -47,6 +40,7 @@ app.use(
 // Mount imported Routers
 
 app.use(jobs);
+app.use(users);
 // app.use('/articles',articlesRouter);
 
 /*** Routes ***/
@@ -55,7 +49,7 @@ const PORT = process.env.PORT || 5000;
 
 // Start the server to listen for requests on a given port
 app.listen(PORT, () => {
-  console.log(`Jobs => http://localhost:${PORT}`);
+  console.log(Jobs => http://localhost:${PORT});
 });
 
 /*
