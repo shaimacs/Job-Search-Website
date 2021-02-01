e82d333b23532bb2a6565911d4556f34e6fe5ee1
35_Shaima Win_SEI-14 <Shaimacs@yahoo.com>
2021-02-01 18:00:10 +0300

    Create UserDatabase.js

---
+++ Back-end/model/UserDatabase.js
@@ -0,0 +1,15 @@
+module.exports=[
+    {
+        name: "User",
+        type: "user",
+        email:"user@gmail.com",
+        password:"123456",
+    },
+
+    {
+        name: "Admin",
+        type: "admin",
+        email:"admin@gmail.com",
+        password:"235689",
+    }
+]
